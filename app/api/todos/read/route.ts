cce528c019bd7316fe32a30497b4e28683d01271
aliblackeye <ali.blackeye@hotmail.com>
2023-06-12 10:50:10 +0300

    first

--- app/api/todos/read/route.ts
+++ app/api/todos/read/route.ts
@@ -1,3 +1,5 @@
+export const revalidate = 0;
+
 // Next
 import { NextResponse } from "next/server";
 
@@ -9,33 +11,33 @@ import Todos from "@/models/TodoSchema";
 
 // get todo list
 export async function GET(req: Request) {
-	try {
-		await connectDB();
-		const projectList = await Todos.find({});
-		if (!projectList) {
-			return NextResponse.json(
-				{
-					success: false,
-					message: "There is no todo",
-				},
-				{ status: 200 }
-			);
-		}
+  try {
+    await connectDB();
+    const projectList = await Todos.find({});
+    if (!projectList) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "There is no todo",
+        },
+        { status: 200 }
+      );
+    }
 
-		return NextResponse.json(
-			{
-				success: true,
-				data: projectList,
-			},
-			{ status: 200 }
-		);
-	} catch (error) {
-		return NextResponse.json(
-			{
-				success: false,
-				message: "Failed to get todos",
-			},
-			{ status: 500 }
-		);
-	}
+    return NextResponse.json(
+      {
+        success: true,
+        data: projectList,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Failed to get todos",
+      },
+      { status: 500 }
+    );
+  }
 }
